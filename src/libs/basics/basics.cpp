9b53cb1d9afcd4f6f428f079fc03215a2667ee4c
renex <reneczechdev@gmail.com>
2022-07-22 12:16:53 -0300

    ENH: basic behaviour review

--- src/libs/basics/basics.cpp
+++ src/libs/basics/basics.cpp
@@ -6,11 +6,15 @@ using namespace std;
 // Params
 void Basics::funa(int z)
 {
+	cout << "funa"<< endl;	
+
 	z = 1;	// OK change to a is local to funa()
 }
 
 void Basics::funb(const int z)
 {
+	cout << "funb"<< endl;	
+
 	//z = 1;	//Compile error - trying to change a const
 }
 
@@ -18,10 +22,30 @@ void Basics::funb(const int z)
 //se modifica se mofica en la funcion
 void Basics::func(int& z)
 {
+	cout << "func"<< endl;	
+
 	z = 1;	// Change to a is reflected back to caller - so must be assignable
 }
 
 void Basics::fund(const int& z)
 {
+	cout << "fund"<< endl;	
+
 	// z = 1	//Compile error - trying to change a const
+}
+
+void Basics::test_enum()
+{
+	cout << "test_enum"<< endl;	
+	enum {blue, green = 5, GREAT};
+	cout << blue << " " << GREAT<< endl;		
+
+}
+
+void Basics::test_string()
+{
+	string palabra = "hola";
+	cout << palabra.size() << "-" << palabra.length() << endl;	
+
+
 }
--- src/libs/basics/include/basics.h
+++ src/libs/basics/include/basics.h
@@ -1,5 +1,9 @@
 #pragma once
 
+#include <iostream>
+#include <string>
+using namespace std;
+
 class Basics {
 
 public:
@@ -7,4 +11,63 @@ public:
 	void funb(const int z);
 	void func(int& z);
 	void fund(const int& z);
+	void test_enum();
+	void test_string();
+
+};
+
+//Ejemplos clases
+
+class Animal {
+protected:
+	Animal(string petName):petName(petName){}
+public:
+	void animalSound(){ cout << "The animal makes a sound \n";}
+	string animalName(){return petName;}
+private:
+	string petName;
+};
+
+//Derived Class
+class Pig: public Animal
+{
+public:
+	Pig(string petName):Animal(petName){};
+	void animalSound(){ cout << this->animalName() << " says: wee wee \n" ; }
+};
+
+//Derived Class
+class Dog: public Animal
+{
+public:
+	Dog(string petName):Animal(petName){};
+	void animalSound(){ cout << this->animalName() << " says: bow wow \n" ; }
+};
+
+// Friend Class in C++
+
+class ClaseA
+{
+    int i =3;
+    friend class ClaseB;
+};
+
+class ClaseB
+{
+  public:
+    void display(ClaseA &a)
+    {
+        cout<<"The value of i is : " << a.i << endl;
+    }
+};
+
+// Static vs Const member functions
+class ClaseMembFunBehav
+{
+	int val;
+public:
+	ClaseMembFunBehav(int x): val(x){};
+    static void static_fun(){cout << "Accessing a static funct \n";}
+	int const_fun() const {return val;}
+	int tryin_change_val(int mod_val){this->val = mod_val;}
 };
