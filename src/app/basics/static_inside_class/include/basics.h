20096dee62c6da957a409f25b84e098988e3a935
renex <reneczech@gmail.com>
2022-07-24 13:20:46 -0300

    ENH: basics > vector optimization using reserve() & emplace_back() instead of push_back, static inside a class

--- src/app/basics/static_inside_class/include/basics.h
+++ src/app/basics/static_inside_class/include/basics.h
@@ -6,6 +6,7 @@ using namespace std;
 
 struct Entity{
 	//int x, y;
+	// Making var static, there will be only one instance of the var in all over the obj instances of Entity!
 	static int x, y;
 
 	void Print()
--- src/app/basics/static_inside_class/main.cpp
+++ src/app/basics/static_inside_class/main.cpp
@@ -7,17 +7,22 @@ int Entity::y;
 int main() {
 
 	Entity e;
-	e.x = 1;
-	e.y = 2;
+	//there is no sense in writing:
+	//e.x = 1;
+	//e.y = 2;
+	Entity::x = 1;
+	Entity::y = 2;
 	
 	
 	Entity e1;
 	// if vars defined static, they are no longer class members vars
 	//e1 = {3 , 4};
 	
-	e1.x = 3;
-	e1.y = 4;
-	
+	//e1.x = 3;
+	//e1.y = 4;
+	Entity::x = 3;
+	Entity::y = 4;
+
 	cout << "e instance ";
 	e.Print();
 	cout << endl;
