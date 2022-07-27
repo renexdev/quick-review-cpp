3f3a6e0163a274bf03f6185f02a184285942c40f
renex <reneczechdev@gmail.com>
2022-07-27 10:19:51 -0300

    ENH: basics union

--- src/app/basics/union/include/basics.h
+++ src/app/basics/union/include/basics.h
@@ -4,17 +4,42 @@
 
 using namespace std;
 
+
 struct Vector2
 {
-    float x, y;
+	float x, y;
 };
-
+/*
 struct Vector4
 {
-    float x, y, z , w;
+	float x, y, z, w;
+	// funcion print vect2 para vect4 
+	// vect4 son dos vect2
+	Vector2& GetA()
+	{
+		// cast the memory addr of x to a vect2 and then derrefence it
+		return *(Vector2*)&x;
+	}
+};
+*/
+// with union
+struct Vector4
+{
+	union
+	{
+		struct
+		{
+			float x, y, z, w;
+		};
+		struct
+		{
+			Vector2 a, b;
+		};
+	};
 };
 
-void PrintVector2( const Vector2& vec)
-{
-    cout << vec.x << vec.y << endl;
+void PrintVector2(const Vector2& vector){
+    cout << "PrintVector2:" << endl;
+    
+	cout << vector.x << " - " << vector.y << endl;	
 }
--- src/app/basics/union/main.cpp
+++ src/app/basics/union/main.cpp
@@ -1,8 +1,11 @@
 #include "include/basics.h"
 
+
 int main() {
+
 	struct Union{
 		// two different ways of address the same memory
+		// used as type punning
 		union 
 		{
 			float a;
@@ -13,5 +16,15 @@ int main() {
 	Union u;
 	u.a = 2.0f;
 	cout << u.a << " - " << u.b << endl;
+
+	Vector4 v = {1.0f, 4.0f, 6.0f, 10.0f};
+	cout << v.x << " - " << v.y << " - " << v.z << " - " << v.w << endl;
+	PrintVector2(v.a);
+	PrintVector2(v.b);
+	v.x = 8.0f; 
+	v.z = 9.0f; 
+	cout << v.x << " - " << v.y << " - " << v.z << " - " << v.w << endl;
+	PrintVector2(v.a);
+	PrintVector2(v.b);
 	return EXIT_SUCCESS;
 }
